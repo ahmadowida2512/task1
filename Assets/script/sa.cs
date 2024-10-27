74e012a0ccc1731b579b6391067bd89fb48c0037
ahmadowida2512 <aowida2512@gmail.com>
2024-10-27 10:30:20 +0200

    task2

--- Assets/script/sa.cs
+++ Assets/script/sa.cs
@@ -8,34 +8,17 @@ public class sa : MonoBehaviour
     void Start()
     {  
     
-   
-                int sayhh=0;
-        
-   
-         while (sayhh<21)
-{
-
- int Randomn=Random.Range(1,21);
- Debug.Log(Randomn);
- sayhh++;
-   if (sayhh == 5)
-
-continue;
-
-   
-   
-      else if (sayhh == 15){
-Debug.Log("ya lahoyyyyyyyyyyyyyyyyyyyyyyyyyysad213");
-Debug.Log("ya ld");
-break;
+   string[] words={"Cat, Dog, Car, Pizza, Hat, Fish, Tree, Monkey, Ball, Bird"};
+//    string len= words.Length.ToString();
+//  Debug.Log(len);
+ int sayahh=0;
+while (sayahh < words.Length){
+Debug.Log(words[sayahh]);
+sayahh++;
 
 
-
-      };
+      }
 
 }  
-    }
-
-
-    
+   
 }
