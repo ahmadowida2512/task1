cdf85ed3f6c36cbe1f4c892c6ee498ccf1d0d04b
ahmadowida2512 <aowida2512@gmail.com>
2024-10-28 08:42:32 +0200

    newwwwwwwwww

--- Assets/script/sa.cs
+++ Assets/script/sa.cs
@@ -1,50 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class sa : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {  
-                            //Task1
-   string[] words={"Cat, Dog, Car, Pizza, Hat, Fish, Tree, Monkey, Ball, Bird"};
-   string len= words.Length.ToString();
-//  Debug.Log(len);
-  int sayahh=0;
-while (sayahh < words.Length){
-Debug.Log(words[sayahh]);
-sayahh++;
+      // Start is called before the first frame update
+      void Start()
+      {
 
+            string[] words = { "Cat","Dog" , "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+     
+            int RandomNumber=Random.Range(1,9);
+             
+                   string sayahh=words[RandomNumber];
+            int i=0;
+            while (i <13)
+            {
+                 int index=RandomNumber;
+            // Debug.Log(words[index]);
+             i++;
+              Debug.Log(sayahh);
+             if (sayahh==words[RandomNumber]){
+                  Debug.Log("you are vary fune mannnnnnnnnnn");
+                
+                break;
+             }
+            }
+          
+           // Task2
+
+                            int sayhh=0;
+
+
+                     while (sayhh<21)
+            {
+
+             int RandomnNamber=Random.Range(1,21);
+             Debug.Log(RandomnNamber);
+             sayhh++;
+               if (sayhh == 5)
+
+            continue;
+
+
+
+                  else if (sayhh == 15){
+            Debug.Log("ya lahoyyyyyyyyyyyyyyyyyyyyyyyyyy");
+            break;
+
+
+
+                  };
+
+            }
 
       }
-                    //Task2
-   
-                int sayhh=0;
-        
-   
-         while (sayhh<21)
-{
 
- int Randomn=Random.Range(1,21);
- Debug.Log(Randomn);
- sayhh++;
-   if (sayhh == 5)
-
-continue;
-
-   
-   
-      else if (sayhh == 15){
-Debug.Log("ya lahoyyyyyyyyyyyyyyyyyyyyyyyyyy");
-break;
-
-
-
-      };
-
-}
-
-}  
-   
 }
---
+++ Assets/script/zx.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+using System.CodeDom;
+
+public class zx : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+string[] authors = { "Mahesh Chand", "Jeff Prosise", "Dave McCarter", "Allen O'neill",
+"Monica Rathbun", "Henry He", "Raj Kumar", "Mark Prime",
+"Rose Tracey", "Mike Crown" };
+
+// Create a Random object
+
+// Generate a random index less than the size of the array.
+
+// Display the result.
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
