89ee237c1d0d66883666bd8aacf0658f4cfc00c7
ahmadowida2512 <aowida2512@gmail.com>
2024-11-01 16:45:02 +0200

    goodddddddddddddddd jobbbbbbbbbbbbbbbbbbbbbb mannnnnnnnnnnnnnnnnnnnnnnnn

--- Assets/script/sa.cs
+++ Assets/script/sa.cs
@@ -7,54 +7,57 @@ using UnityEngine;
 public class sa : MonoBehaviour
 {
       // Start is called before the first frame update
+     
+         string sayahh;
+
       void Start()
       {
 
-            string[] words = { "Cat","Dog" , "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
-     
-            int RandomNumber=Random.Range(1,9);
-             
-                   string sayahh=words[RandomNumber];
-            int i=0;
-            while (i <13)
+            string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+
+
+            int i = 1;
+            while (i <= 7)
             {
-                 int index=RandomNumber;
-            // Debug.Log(words[index]);
-             i++;
-              Debug.Log(sayahh);
-             if (sayahh==words[RandomNumber]){
-                  Debug.Log("you are vary fune mannnnnnnnnnn");
-                
-                break;
-             }
+                  int RandomNumber = Random.Range(0, 10);
+                  sayahh+=  words[RandomNumber]+",";
+                  
+                  i++;
+
+                  //  if (sayahh==words[RandomNumber]){
+                  //       Debug.Log("you are vary fune mannnnnnnnnnn");
+
+                  //     break;
+                  //  }
             }
-          
-           // Task2
+            
+Debug.Log(sayahh);
+            // Task2
 
-                            int sayhh=0;
-
-
-                     while (sayhh<21)
-            {
-
-             int RandomnNamber=Random.Range(1,21);
-             Debug.Log(RandomnNamber);
-             sayhh++;
-               if (sayhh == 5)
-
-            continue;
+            //                 int sayhh=0;
 
 
+            //          while (sayhh<21)
+            // {
 
-                  else if (sayhh == 15){
-            Debug.Log("ya lahoyyyyyyyyyyyyyyyyyyyyyyyyyy");
-            break;
+            //  int RandomnNamber=Random.Range(1,21);
+            //  Debug.Log(RandomnNamber);
+            //  sayhh++;
+            //    if (sayhh == 5)
+
+            // continue;
 
 
 
-                  };
+            //       else if (sayhh == 15){
+            // Debug.Log("ya lahoyyyyyyyyyyyyyyyyyyyyyyyyyy");
+            // break;
 
-            }
+
+
+            //       };
+
+            // }
 
       }
 
